5aef4776eec18b3b80ba6653c9df0e7f0a363db9
machhy21 <machhy21@sps-prosek.cz>
2024-11-25 16:06:34 +0100

    commit25_11

--- student-packages/lecture-1/main.c
+++ student-packages/lecture-1/main.c
@@ -2,7 +2,12 @@
 #include "pico/stdlib.h"
 
 #define LED_PIN 6
-#define BLINK_DELAY_MS 500
+#define BLINK_DELAY_MS_SHORT 100
+#define BLINK_DELAY_MS_LONG 300
+
+//function
+void dot();
+//void 
 
 int main()
 {
@@ -13,13 +18,37 @@ int main()
     // Infinite loop
     while (1)
     {
-        // Turn on the LED
-        gpio_put(LED_PIN, 1);
-        sleep_ms(BLINK_DELAY_MS);
+        for(int i = 0; i < 3; i++){
+            // Turn on the LED
+            gpio_put(LED_PIN, 1);
+            sleep_ms(BLINK_DELAY_MS_SHORT);
 
-        // Turn off the LED
-        gpio_put(LED_PIN, 0);
-        sleep_ms(BLINK_DELAY_MS);
+            // Turn off the LED
+            gpio_put(LED_PIN, 0);
+            sleep_ms(BLINK_DELAY_MS_SHORT);
+        }
+
+        for(int i = 0; i < 3; i++){
+            // Turn on the LED
+            gpio_put(LED_PIN, 1);
+            sleep_ms(BLINK_DELAY_MS_LONG);
+
+            // Turn off the LED
+            gpio_put(LED_PIN, 0);
+            sleep_ms(BLINK_DELAY_MS_LONG);
+        }
+
+        for(int i = 0; i < 3; i++){
+            // Turn on the LED
+            gpio_put(LED_PIN, 1);
+            sleep_ms(BLINK_DELAY_MS_SHORT);
+
+            // Turn off the LED
+            gpio_put(LED_PIN, 0);
+            sleep_ms(BLINK_DELAY_MS_SHORT);
+        }
+        sleep_ms(BLINK_DELAY_MS_LONG);
+        
     }
     return 0;
 }
--- student-packages/lecture-2/main.c
+++ student-packages/lecture-2/main.c
@@ -8,9 +8,14 @@
 
 #define LED_PIN 6
 #define BUTTON_PIN 20
+#define BUTTON1_PIN 21
+
+void init();
 
 void handler(uint gpio, uint32_t events);
 uint32_t millis();
+
+int number = 0;
 
 uint32_t lastIrgTime = 0;
 
@@ -22,21 +27,18 @@ int main()
     sleep_ms(2000);
     printf("Starting...\n");
 
-    // Initialize GPIO pins
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-
-    gpio_init(BUTTON_PIN);
-    gpio_pull_up(BUTTON_PIN);
-    gpio_set_dir(BUTTON_PIN, GPIO_IN);
+    init();
 
     // Enable interrupts for button pin
     gpio_set_irq_enabled_with_callback(BUTTON_PIN, GPIO_IRQ_EDGE_RISE, true, &handler);
+    gpio_set_irq_enabled_with_callback(BUTTON1_PIN, GPIO_IRQ_EDGE_RISE, true, &handler);
+ 
 
     // Infinite loop
     while (1)
     {
-        printf("Run...\n");
+        printf("\rRun...");
+        printf("%d\n", number);
         sleep_ms(1000);
     }
     return 0;
@@ -47,9 +49,30 @@ void handler(uint gpio, uint32_t events)
     if ((millis() - lastIrgTime) >= 200) {
         gpio_put(LED_PIN, !gpio_get(LED_PIN));
         lastIrgTime = millis();
+        if(gpio == BUTTON_PIN){
+           number++; 
+        }else if(gpio == BUTTON1_PIN){
+            number--;
+        }
     }
 }
 
 uint32_t millis() {
     return to_ms_since_boot(get_absolute_time());
+}
+
+
+
+void init(){
+    // Initialize GPIO pins
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    gpio_init(BUTTON_PIN);
+    gpio_pull_up(BUTTON_PIN);
+    gpio_set_dir(BUTTON_PIN, GPIO_IN);
+
+    gpio_init(BUTTON1_PIN);
+    gpio_pull_up(BUTTON1_PIN);
+    gpio_set_dir(BUTTON1_PIN, GPIO_IN);
 }
